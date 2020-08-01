76bbc486a5341ec06c2eb0120e790c4118208055
yeonjudkwl <hyj9785@likelion.org>
2020-08-01 22:05:54 +0900

    router | page not found

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '*',
+    redirect: '/404',
+  },
+  {
+    path: '/404',
+    component: () => import('@/views/NotFoundPage.vue'),
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/views/NotFoundPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1>page not found</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
