454791cf0b5843d1e3ef4b0498688129d12d268e
yeonjudkwl <hyj9785@likelion.org>
2020-08-02 17:20:54 +0900

    axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,12 +8,36 @@
         <router-link to="/board/2">Board 2</router-link>
       </li>
     </ul>
-    
+    <div v-if="loading">Loading...</div>
+    <div v-else>{{ boards }}</div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Home',
+  data () {
+    return {
+      loading: false,
+      boards: [],
+    }
+  },
+  methods: {
+    fetchData () {
+      this.loading = true
+      axios.get('http://localhost:3000/boards')
+        .then( res => {
+          this.boards = res.data
+        })
+        .catch( () => {
+          this.$router.replace('/login')
+        })
+        .finally( () => {
+          this.loading = false
+        })
+    }
+  }
 }
 </script>
