e0c466d14aeed488ac64a4146b4b511af5e47e87
yeonjudkwl <hyj9785@likelion.org>
2020-08-09 14:18:51 +0900

    delete board

--- src/components/BoardSettings.vue
+++ src/components/BoardSettings.vue
@@ -5,20 +5,31 @@
       <a class="header-close-btn" href="" @click.prevent="onClose">&times;</a>
     </div>
     <ul class="menu-list">
-      <li>Menu 1</li>
+      <li><a href="" @click.prevent="onDeleteBoard">Delete Board</a></li>
     </ul>
   </div>
 </template>
 
 <script>
-import {mapMutations} from 'vuex'
+import {mapMutations, mapActions, mapState} from 'vuex'
+
 export default {
+  computed: {
+      ...mapState(['board'])
+  },
   methods: {
     ...mapMutations([
       'SET_IS_SHOW_BOARD_SETTINGS'
     ]),
+    ...mapActions(['DELETE_BOARD']),
     onClose() {
       this.SET_IS_SHOW_BOARD_SETTINGS(false)
+    },
+    onDeleteBoard () {
+        if (!window.confirm(`${this.board.title}을 삭제하시겠습니까?`)) return
+        this.DELETE_BOARD({id: this.board.id})
+            .then( () => this.SET_IS_SHOW_BOARD_SETTINGS(false))
+            .then( () => this.$router.push('/'))
     }
   }
 }
